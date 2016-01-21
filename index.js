63373221ef34a306e60a00ffe2db693df354d872
shiva subedi <trsubedi@hotmail.com>
2016-01-21 15:04:20 -0800

    boiler plate set up

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 //requirements
 var express = require('express'),
-	app = express();
+	path = require("path");
+
+var	app = express(),
+	views = path.join(__dirname, "views");
 
 app.get("/", function (req, res) {
 	res.send(" This site is under Constuction");
 });
 
-
+app.get("/home", function(req, res){
+	var homePath = path.join(views, "index.html");
+	res.sendFile(homePath);
+});
 
 
 //start the server
