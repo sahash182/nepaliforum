6e04e053b10ba5f9b5414f5b6c372995d0b251cb
shiva subedi <trsubedi@hotmail.com>
2016-01-21 15:43:06 -0800

    models added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 //requirements
 var express = require('express'),
+	mongoose = require('mongoose'),
+  	db = require('./models'),
 	path = require("path");
 
 var	app = express(),
 	views = path.join(__dirname, "views");
+
+// assets
+app.use("/static", express.static("public"));
+app.use("/vendor", express.static("bower_components"));
 
 app.get("/", function (req, res) {
 	res.send(" This site is under Constuction");
